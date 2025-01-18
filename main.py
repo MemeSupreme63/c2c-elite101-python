8f4be764e24eaaa472daf8de206283e2edf95b1f
MemeSupreme63 <iain@lolgames.org>
2025-01-18 19:50:45 +0000

    I'm commiting like its a relationship

--- main.py
+++ main.py
@@ -12,16 +12,40 @@
         When the script is run directly (not imported as a module), it will execute the main() function.
 """
 
-
+userName = "user"
 def get_user_name():
     return input("Please enter your name: ")
 
 def greet_user(name):
     print(f"Hello, {name}!")
+    userName = name
+def getUserAge():
+    return input(f"How old are you, {userName}?")
+def handleOption(option):
+    if option == 1:
+        print("You picked the first option")
+    elif option == 2:
+        print("You picked the second option")
+    elif option == 3:
+        print("You picked the third option")
+    elif option == 4:
+        print("You picked the final option")
+def menu():
+    print(f"Please pick an option, {userName}")
+    print("1: Placeholder")
+    print("2: Placeholder again")
+    print("3: Another placeholder")
+    print("4: The last placeholder")
+    option = int(input())
+    handleOption(option)
 
 def main():
     user_name = get_user_name()
     greet_user(user_name)
+    age = getUserAge()
+    print(f"Man... {age} is old asf")
+    menu()
+
 
 if __name__ == "__main__":
     main()
